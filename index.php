9f9ba782afceb8eb8680dab558237276cf84d37c
Luis <s.ros.lui@gmail.com>
2021-01-05 11:07:50 +0100

    add stile e struttura base pagina

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link foglio stile css -->
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Php Dischi</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img src="./img/logo.png" alt="logo spotify">
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="box-disco">
+                img
+                <h4>titolo canzone</h4>
+                <div>cantante</div>
+                <div>anno disco</div>
+                <div>genere musica</div>
+            </div>
+
+            
+        </div>
+    </main>
+</body>
+</html>
