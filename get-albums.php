86a04c140bd4ce95296c541a585fd7b6c315a571
Luis <s.ros.lui@gmail.com>
2021-01-06 11:27:29 +0100

    add get albums & get artist

--- get-albums.php
+++ get-albums.php
@@ -1,8 +1,43 @@
 <?php
-// get database (richiedo il database)
-require_once __DIR__ . '/./partials/database.php';
+/* get database (richiedo il database)*/
 // RICORDA CHE: con DIR mi riporta al file attuale
+require_once __DIR__ . '/./partials/database.php';
+
+/* filtraggio dati in base alle keyword query*/
+
+/* GET ALBUMS*/
+// filtro in base agli 'album' 
+$artist = empty($_GET['artist']) ? false : $_GET['artist'];
+$albums = [];
+
+if($artist == false || $artist == 'all'){ // settando 'all' oppure  non metto nulla
+    $albums = $database;                 //ottenendo tutto il databse
+                            
+}else{
+    foreach($database as $cd){
+        if(cd['author'] == $artist){
+            $albums = $cd;  // stampa l'album corrisposto
+        }
+    }
+}
+
+/*GET ARTIST */
+// filtro in base agli artisti
+
+$artists = [];
+foreach($database as $cd){
+    if(! in_array($cd['author'], $artist) ){
+        $artists[] = $cd['author'];
+    }
+}
+
+// FINAL RESULTS
+// stampa l'array finale contenente le chiavi 'albums' e 'artists'
+$response = [
+    'albums' => $albums,
+    'artists' => $artists
+];
 
 // output
 header('Content-type: application/json');
-echo json_encode($database);
+echo json_encode($response);
